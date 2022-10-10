45a9defb8a8046ec971a51ecfe7042f3e82ad599
migenjutsu <domhenin.517@gmail.com>
2022-10-10 08:19:32 -0400

    still wip: update vpc_route

---
+++ aws_vpc_routing_vars.tf
@@ -0,0 +1,9 @@
+variable "aws_internet_route_cidr_destination_module" {
+    description = "for module igw cidr destionation"
+    type = string    
+}
+
+variable "aws_internet_route_name_module" {
+    description = "for module igw name"
+    type = string  
+}
